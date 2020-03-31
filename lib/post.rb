5febde4633ac85e96216f791fd9eb47dab553701
Dylan Williamson <dirtyassweed@icloud.com>
2020-03-31 19:59:30 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,15 +1,5 @@
 class Post
 
   attr_accessor :title, :author
-  def initialize(title)
-    @title = title
-  end
 
-  def author_name
-    if @author == nil
-      return nil
-    else
-      @author.name
-    end
-  end
 end
