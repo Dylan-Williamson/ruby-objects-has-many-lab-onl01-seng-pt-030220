b6d707e1a07320ad348cea1cfef89dbed4d562de
Dylan Williamson <dirtyassweed@icloud.com>
2020-03-31 20:02:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,23 @@
 class Song
-  attr_accessor :title, :artist
+
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    artist.name if artist
+  end
 end
